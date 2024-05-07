62874fa18a5a2b286ea6652340b64c4124b48c26
Rafael Martins Porto <rafael.m.porto@aluno.senai.br>
2024-05-07 14:12:47 -0300

    vv

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 function alterarPagina(){
-    
+    let fundo = document.querySelector("body");
+    if (caixa.classList.contains("box-escuro")){
+        caixa.classList.remove("box-escuro");
+    }
+    else{
+        caixa.classList.add("box-escuro");
+    }
 }
